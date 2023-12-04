7070d3ee53a46ce6fa38f028ab855dec1b238a20
Francesco <francescocollotto@gmail.com>
2023-12-04 14:27:02 +0000

    add basic seed data

--- Domain/Data/DataContext.cs
+++ Domain/Data/DataContext.cs
@@ -9,4 +9,26 @@ public class DataContext : DbContext
   public DbSet<Airport> Airports { get; set; }
   public DbSet<Country> Countries { get; set; }
   public DbSet<AirportType> AirportTypes { get; set; }
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    modelBuilder.Entity<AirportType>().HasData(
+      new AirportType { Id = 1, Type = "Departure and Arrival" },
+      new AirportType { Id = 2, Type = "Departure Only" },
+      new AirportType { Id = 3, Type = "Arrival Only" }
+    );
+
+    modelBuilder.Entity<Country>().HasData(
+      new Country { Id = 1, Name = "United Kingdom" },
+      new Country { Id = 2, Name = "Spain" },
+      new Country { Id = 3, Name = "United States" },
+      new Country { Id = 4, Name = "Turkey" }
+    );
+
+    modelBuilder.Entity<Airport>().HasData(
+      new Airport { Id = 1, IATACode = "LGW", CountryId = 1, AirportTypeId = 1 },
+      new Airport { Id = 2, IATACode = "PMI", CountryId = 2, AirportTypeId = 3 },
+      new Airport { Id = 3, IATACode = "LAX", CountryId = 3, AirportTypeId = 3 }
+    );
+  }
 }
---
+++ Migrations/20231204142352_SeedBasicModels.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace IntuitiveTechTest.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedBasicModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AirportTypes",
+                columns: new[] { "Id", "Type" },
+                values: new object[,]
+                {
+                    { 1, "Departure and Arrival" },
+                    { 2, "Departure Only" },
+                    { 3, "Arrival Only" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "United Kingdom" },
+                    { 2, "Spain" },
+                    { 3, "United States" },
+                    { 4, "Turkey" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Airports",
+                columns: new[] { "Id", "AirportTypeId", "CountryId", "IATACode" },
+                values: new object[,]
+                {
+                    { 1, 1, 1, "LGW" },
+                    { 2, 3, 2, "PMI" },
+                    { 3, 3, 3, "LAX" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AirportTypes",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Airports",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Airports",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Airports",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "AirportTypes",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "AirportTypes",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
